e5b42a874e01b215b24639ed0eaca9efdb6b6caf
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-05-31 06:36:55 +0200

    Add files via upload

---
+++ Storage_Account.ps1
@@ -0,0 +1,22 @@
+﻿Set-Location c:\
+Clear-Host
+
+Install-Module -Name Az -Force -AllowClobber -Verbose
+
+Connect-AzAccount
+
+# Are we connected?
+Get-AzResourceGroup
+
+Get-AzLocation | select Location
+$location = "westeurope"
+
+# Create a resource group
+$resourceGroup = "myResourceGroup1975"
+New-AzResourceGroup -Name $resourceGroup -Location $location
+
+# Create a storage account
+$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroup `
+  -Name "twstorageazure" `
+  -SkuName Standard_LRS `
+  -Location $location
